e105fc57899df5620e146c7f4d3abc6fc4045f24
Drashtee <drashteeparmar@gmail.com>
2022-01-22 09:20:02 -0500

    added link to html aand db

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const path = require('path');
-const database = require('./db/db.json')
+const database = require('./db/db')
 
 /* Setting express port */
 const PORT = process.env.PORT || 3001;
