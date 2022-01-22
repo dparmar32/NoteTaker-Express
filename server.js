1dd2d3b5090633fff54d966b4a33c89d62c41633
Drashtee <drashteeparmar@gmail.com>
2022-01-22 18:46:40 -0500

    get works with no error in console

--- server.js
+++ server.js
@@ -1,24 +1,36 @@
-const express = require('express');
-const path = require('path');
-const fs = require('fs');
-const uuid = require('uuid');
-const { notes } = require('./db/db.json')
-
+const express = require("express");
+const path = require("path");
+const fs = require("fs");
 
 /* Setting express port */
 const PORT = process.env.PORT || 3001;
 const app = express();
 
-
-/* Enabling link to given folder for accessing info */
-app.use(express.static('public'));
-app.use(express.urlencoded({extended: true}));
+app.use(express.static("./public"));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.get('/api/notes',(req, res) => res.json(notes));
-app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, './public/notes.html')));
+/* GET: for retrieving the notes */
+app.get("/api/notes",
+    (req, res) => {
+        fs.readFile("./db/db.json", (err, data) => {
+            if (!err) {
+                var notes = JSON.parse(data);
+                res.json(notes);
+            } else {
+                throw err;
+            }
+        });
+    });
 
+app.get("/notes",
+    (req, res) => {
+        res.sendFile(path.join(__dirname, "./public/notes.html"));
+    });
 
+app.get("*",
+    (req, res) => {
+        res.sendFile(path.join(__dirname, "./public/index.html"));
+    });
 
 app.listen(PORT, () => console.log(`App listening on port ${PORT}`));
-
